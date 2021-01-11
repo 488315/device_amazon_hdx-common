5c7260a1743ec2f6ba3246c07884603f71b71057
Joel Bacchus <joel.bacchus@protonmail.com>
2021-01-11 17:18:10 +0000

    camera: convert camera manifest and framework to hwbinder

--- hidl.mk
+++ hidl.mk
@@ -12,7 +12,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service
 
 # DRM
 PRODUCT_PACKAGES += \
