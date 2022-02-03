d34fbba324c420b8b5ceb64ed0dcaffa869bb34b
Sergey Nikiforov <void234@gmail.com>
2022-02-03 08:27:55 +0000

    patches: wifi: various fixes for HDX

--- patches/apply.sh
+++ patches/apply.sh
@@ -2,7 +2,7 @@ echo $1
 rootdirectory="$PWD"
 # ---------------------------------
 
-dirs="frameworks/base system/bt"
+dirs="frameworks/base frameworks/opt/net/wifi packages/apps/Settings system/bt"
 
 for dir in $dirs ; do
 	cd $rootdirectory
